66c6487f635d186da3bb0921f5075e3acbfa2a4e
Sherhang <Sherhang@github.com>
2019-07-28 20:26:11 +0800

    create

---
+++ demo2.cpp
@@ -0,0 +1,39 @@
+/*基本使用
+ * */
+#include<iostream>
+#include"redis.h"
+#include<algorithm>
+#include<iterator>
+using namespace std;
+int main()
+{
+    Redis redis;
+    string ip="127.0.0.1";
+    int port = 6379;
+    string password="yehang0201";
+    redis.init(ip, port, password);
+    redis.connect();
+    vector<string> keys={"str1","str2","str3","str4","str5"};
+    vector<string> values={"1s","2s","3s","4s","5s"};
+    redis.mset(keys,values);
+    keys.clear();
+    redis.keys("str?", keys);
+    sort(keys.begin(),keys.end());
+    cout<<"all keys are:"<<endl;
+    copy(keys.begin(),keys.end(),ostream_iterator<string>(cout," "));
+    cout<<endl;
+    vector<string> v;
+    redis.mget(keys,v);
+    cout<<"values ars"<<endl;
+    copy(v.begin(), v.end(),ostream_iterator<string>(cout, " "));cout<<endl;
+    cout<<"delete all keys like str?"<<endl;
+    for(int i=0;i<keys.size();++i)
+    {
+        redis.del(keys[i]);
+    }
+    string key = "str1";
+    string t = redis.type(key);
+    cout<<"type of str1 is "<<t<<endl;
+    redis.disconnect();
+    redis.disconnect();//重复dis看看
+}
---
+++ demo4.cpp
@@ -0,0 +1,64 @@
+/*redis_connect_pool测试
+ *
+ * */
+
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+
+#include"redis.h"
+#include"redis_connect_pool.h"
+
+using namespace std;
+
+int main()
+{   int pool_size = 2;
+    string ip = "127.0.0.1";
+    int port = 6379;
+    string passwd ="yehang0201";
+    RedisConnectPool rc_pool;
+    if(!rc_pool.init(pool_size, ip, port, passwd))
+    {
+        cout<<"rc_pool init failure!"<<endl;
+        exit(0);
+    }
+    cout<<"used num : "<<rc_pool.getUsedCount()<<endl;
+    cout<<"canUse num : "<<rc_pool.getConnectedNum()<<endl;
+    
+    //1
+    cout<<"used num : "<<rc_pool.getUsedCount()<<endl;
+    cout<<"canUse num : "<<rc_pool.getConnectedNum()<<endl;
+    rc_pool.getConnect();
+    
+    //2
+    cout<<"used num : "<<rc_pool.getUsedCount()<<endl;
+    cout<<"canUse num : "<<rc_pool.getConnectedNum()<<endl;
+    redisContext* context = rc_pool.getConnect();
+    rc_pool.releaseConnect(context);//回收
+
+    //3
+    cout<<"used num : "<<rc_pool.getUsedCount()<<endl;
+    cout<<"canUse num : "<<rc_pool.getConnectedNum()<<endl;
+    context = rc_pool.getConnect();
+    Redis redis(context);//初始化，这种方式会自动禁用disconnect
+    redis.set("str", "hello");
+    string v;
+    v = redis.get("str");
+    cout<<"get key "<<v<<endl;
+    redis.disconnect();//这句话无效
+    rc_pool.releaseConnect(context);//回收
+    
+    //4
+    cout<<"used num : "<<rc_pool.getUsedCount()<<endl;
+    cout<<"canUse num : "<<rc_pool.getConnectedNum()<<endl;
+    context = rc_pool.getConnect();    
+    if(context==NULL)
+        cout<<"no context"<<endl;
+    Redis redis1(context);
+    cout<<redis1.get("str")<<endl;
+
+    rc_pool.releaseConnect(context);
+    rc_pool.releaseConnect(context);//重复回收
+    cout<<"used num : "<<rc_pool.getUsedCount()<<endl;
+    cout<<"canUse num : "<<rc_pool.getConnectedNum()<<endl;
+}
